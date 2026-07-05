ddedeeec345d303c48b324f898e57928230666ae
abdulmalikbaabed-oss <abdulmalikbaabed@gmail.com>
2026-07-05 19:29:13 +0800

    Delete secrets.py

--- secrets.py
+++
@@ -1,6 +0,0 @@
-secrets = {
-    "ssid": "mk",
-    "password": "00000000",
-    "blynk_token": "xsEo95FqRH9vZ9GjlrqXUBGH_u59jFzF",
-    "google_script_id": "AKfycbyHDSnCPkcYrW5-D0rinqM4pRbWeY6Msz0xnuZvoeVsnuCDffZsIJsQ9wzK1EGTimUc"
-}
